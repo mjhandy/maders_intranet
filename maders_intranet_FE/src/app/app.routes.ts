01c316e6f0925346b327f2c87877ff23b8f651f4
Mark J. Handy <mjhandy@outlook.com>
2026-03-06 10:37:35 -0400

    Code clean up and base header and footer

--- maders_intranet_FE/src/app/app.routes.ts
+++ maders_intranet_FE/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    loadComponent: () => import('./components/pages/home/home').then(m => m.Home),
+    title: "Home Page | Mader's Intranet",
+    data: {
+      description: "Mader's Internet Home Page",
+      label: 'Home',
+      showInMain: false,
+      showInFooter: true,
+      h1: 'Digital Visibility. <br>Human Connetion.',
+      h2: 'We craft inclusive digital experiences that don’t just perform, they resonate.',
+      desktopImage: 'castle-loch-bridge.webp',
+      mobileImage: 'castle-loch-bridge.webp',
+      headerPostion: 'left',
+      ctaCopy: 'Book An Audit',
+      ctaTarget: 'contact-us'
+    }
+  },
+];
--- maders_intranet_FE/src/app/app.ts
+++ maders_intranet_FE/src/app/app.ts
@@ -1,9 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Header } from './components/global/header/header';
+import {Footer } from './components/global/footer/footer';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Header,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
